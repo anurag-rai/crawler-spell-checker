a0fb413ef39eb7f4f3671113b4a6eb51985f6688
Anurag Rai <7anuragrai7@gmail.com>
2018-10-31 01:36:57 +0530

    Removing commented code

--- content.js
+++ content.js
@@ -1,9 +1,5 @@
 // By default, Chrome injects content scripts after the DOM is complete.
 
-// var firstHref = $("a");
-// console.log(" >> ", firstHref);
-
-// console.log(" >>>> The first link on the site is: " , firstHref);
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
@@ -13,25 +9,11 @@ chrome.runtime.onMessage.addListener(
 );
 
 var masterLinks = {};
-
-// var level1hyperlinks = {};
-// var level2hyperlinks = {};
-
-// var level1pages = [];
-
 var wrongWords = {};
 
 var checkValue = function(value){
   return masterLinks[value] === true;
 };
-
-// var checkValue = function(value){
-//   return level1hyperlinks[value] === true;
-// };
-
-// var checkValue2 = function(value){
-//   return level2hyperlinks[value] === true;
-// };
 
 function populateFirstLinks() {
 	console.log("Populating first links ... ");
@@ -45,61 +27,6 @@ function populateFirstLinks() {
    });
    return links;
 }
-
-// async function getPages(level) {
-// 	if ( level === 1 ) {
-// 		await getPagesFromLinks(level1hyperlinks);
-// 	}else if ( level === 2 ) {
-// 		await getPagesFromLinks(level2hyperlinks);
-// 	}
-// }
-
-// // param:
-// // hyperlinks = { link1 : true, link2 : true .... }
-// function getPagesFromLinks(hyperlinks) {
-// 	var keys = Object.keys(hyperlinks);
-// 	console.log(" Keys: " , keys);
-// 	var arrayLength = keys.length;
-// 	jQuery.ajaxSetup({async:false});
-// 	for (var i = 0; i < arrayLength; i++) {
-// 	    $.get(keys[i], function(response) {
-// 		  // console.log("Nested: ", response);
-// 		  // console.log($(response).text());
-// 		  // getText($(response).text());
-// 		  level1pages.push(response);
-// 		});	
-// 	}
-// 	// console.log("Pages: ", pages);
-// 	// for ( key in keys ) {
-// 	// 	$.get(key, function(response) {
-// 	// 	  console.log("Nested: ", response);
-// 	// 	});	
-// 	// }
-// }
-
-// // param:
-// // pages = [page1, page2, page3]
-// function getLinksFromPages(pages) {
-// 	console.log("Getting links from pages: ")
-// 	var arrayLength = pages.length;
-// 	for (var i = 0; i < arrayLength; i++) {
-// 		console.log("Page: ", pages[i]);
-// 	    // level2hyperlinks = get 'a' from each page
-// 	    console.log(" $Page : ", $(pages[i]) );
-// 	 //    var xmlString = $(pages[i])
-// 		//   , parser = new DOMParser()
-// 		//   , doc = parser.parseFromString(xmlString, "text/html");
-// 		// // doc.firstChild // => <div id="foo">...
-// 		// // doc.firstChild.firstChild
-// 	 //    console.log(" HREFS: ", doc.getElementById("a"));
-// 	 	console.log(" HREFS: ", $(pages[i]).find("a").each(function() {
-// 	   		// if ( !checkValue(this.href) && this.href !== "" && /^http/.test(this.href)) {
-// 	   		if ( !checkValue2(this.href) && this.href !== "") {
-// 	     	 	level2hyperlinks[this.href] = true;
-// 	      	}
-// 	 	}));
-// 	}
-// }
 
 // given an array of words, checks spelling
 function checkSpelling(words) {
@@ -152,19 +79,6 @@ function populateError(data) {
 		console.log("Wrong words: ", wrongWords);
 	}
 }
-// // get words from the page text and checks spelling
-// function getText(currentPageText) {
-// 	console.log("Current page: ", currentPageText);
-// 	// var html = currentPage;
-// 	// var usingHtmlPage = currentPage.getElementById("paragraph").innerHTML;
-// 	// var x = $('p').text();
-// 	// console.log("p tags: ", x);
-// 	// console.log("using html: ", usingHtmlPage);
-// 	var r = /([a-zA-Z]+)/g;
-// 	var parsed = currentPageText.match(r);
-// 	console.log(parsed);
-// 	checkSpelling(parsed);
-// }
 
 function solve(links) {
 	getPages(links, 1);
@@ -241,19 +155,3 @@ function startCrawlAndSpellCheck(event) {
 
 	solve(firstLinks);
 }
-// // get all links (second level) in the first page
-// getAllLinks();
-
-// getPages(1).then(function whenOk() {
-// 	console.log("level1pages: " ,level1pages);
-// 	getLinksFromPages(level1pages);
-// 	console.log("level2links : ", level2hyperlinks);
-// });
-
-// // spell checks on the home page
-// // getText($('html').text());
-
-
-// console.log(" >> " , level1hyperlinks);
-// console.log("Document: ", document);
-// document.getElementById('trigger').addEventListener('click', triggered);
