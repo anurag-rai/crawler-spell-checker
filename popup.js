863a9bc7b70a2dbb15875c57770ff7dad19ef5ae
Anurag Rai <7anuragrai7@gmail.com>
2018-10-31 01:40:04 +0530

    Removing commented code

--- popup.js
+++ popup.js
@@ -1,23 +1,5 @@
 // 'use strict';
 
-// function calculate(event) {
-//   let minutes = parseFloat(event.target.value);
-//   chrome.browserAction.setBadgeText({text: 'ON'});
-//   chrome.alarms.create({delayInMinutes: minutes});
-//   chrome.storage.sync.set({minutes: minutes});
-//   window.close();
-// }
-
-// function clearAlarm() {
-//   chrome.browserAction.setBadgeText({text: ''});
-//   chrome.alarms.clearAll();
-//   window.close();
-// }
-
-// //An Alarm delay of less than the minimum 1 minute will fire
-// // in approximately 1 minute incriments if released
-// document.getElementById('trigger').addEventListener('click', setAlarm);
-
 function triggered(event) {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 	    var activeTab = tabs[0];
